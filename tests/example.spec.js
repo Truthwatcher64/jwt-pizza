85b3fd700659931c57716b1314e0f280af134ad4
Alex B <49875526+Truthwatcher64@users.noreply.github.com>
2024-10-03 16:12:51 -0600

    Removed jest as a test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,3 +5,8 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test('login test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('main')).toContainText('Pizza is an absolute delight that brings joy to people of all ages. The perfect combination of crispy crust, savory sauce, and gooey cheese makes pizza an irresistible treat. At JWT Pizza, we take pride in serving the web\'s best pizza, crafted with love and passion. Our skilled chefs use only the finest ingredients to create mouthwatering pizzas that will leave you craving for more. Whether you prefer classic flavors or adventurous toppings, our diverse menu has something for everyone. So why wait? Indulge in the pizza experience of a lifetime and visit JWT Pizza today!');
+});
