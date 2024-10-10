617c454e9aef786cb50bc5f52856ed8d5866b921
Alex B <49875526+Truthwatcher64@users.noreply.github.com>
2024-10-10 12:41:46 -0600

    added a spot for logout

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -9,10 +9,10 @@ test('login test', async ({ page }) => {
     await route.fulfill({ json: loginRes });
   });
 
-await page.goto('http://localhost:5173/');
-await expect(page.getByText('The web\'s best pizza', { exact: true })).toBeVisible();
-await expect(page.getByRole('button', { name: 'Order now' })).toBeVisible();
-await expect(page.getByRole('main')).toContainText('Pizza is an absolute delight that brings joy to people of all ages. The perfect combination of crispy crust, savory sauce, and gooey cheese makes pizza an irresistible treat. At JWT Pizza, we take pride in serving the web\'s best pizza, crafted with love and passion. Our skilled chefs use only the finest ingredients to create mouthwatering pizzas that will leave you craving for more. Whether you prefer classic flavors or adventurous toppings, our diverse menu has something for everyone. So why wait? Indulge in the pizza experience of a lifetime and visit JWT Pizza today!');
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByText('The web\'s best pizza', { exact: true })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Order now' })).toBeVisible();
+  await expect(page.getByRole('main')).toContainText('Pizza is an absolute delight that brings joy to people of all ages. The perfect combination of crispy crust, savory sauce, and gooey cheese makes pizza an irresistible treat. At JWT Pizza, we take pride in serving the web\'s best pizza, crafted with love and passion. Our skilled chefs use only the finest ingredients to create mouthwatering pizzas that will leave you craving for more. Whether you prefer classic flavors or adventurous toppings, our diverse menu has something for everyone. So why wait? Indulge in the pizza experience of a lifetime and visit JWT Pizza today!');
 });
 
 test('purchase with login', async ({ page }) => {
@@ -106,4 +106,9 @@ test('purchase with login', async ({ page }) => {
 
   // Check balance
   await expect(page.getByText('0.008')).toBeVisible();
+});
+
+
+test('logout path', async ({ page }) => {
+
 });
