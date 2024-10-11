d77d6773e051fd7c35801d254a206337645fabb2
Truthwatcher64 <TruthwatcherAB@protonmail.com>
2024-10-10 20:00:52 -0600

    Added the logout tests 66% coverage

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,6 +13,14 @@ test('login test', async ({ page }) => {
   await expect(page.getByText('The web\'s best pizza', { exact: true })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Order now' })).toBeVisible();
   await expect(page.getByRole('main')).toContainText('Pizza is an absolute delight that brings joy to people of all ages. The perfect combination of crispy crust, savory sauce, and gooey cheese makes pizza an irresistible treat. At JWT Pizza, we take pride in serving the web\'s best pizza, crafted with love and passion. Our skilled chefs use only the finest ingredients to create mouthwatering pizzas that will leave you craving for more. Whether you prefer classic flavors or adventurous toppings, our diverse menu has something for everyone. So why wait? Indulge in the pizza experience of a lifetime and visit JWT Pizza today!');
+
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('Email address').click();
+  await page.getByPlaceholder('Email address').fill('d@jwt.com');
+  await page.getByPlaceholder('Email address').press('Tab');
+  await page.getByPlaceholder('Password').fill('a');
+
 });
 
 test('purchase with login', async ({ page }) => {
@@ -79,6 +87,7 @@ test('purchase with login', async ({ page }) => {
 
   await page.goto('http://localhost:5173/');
 
+  
   // Go to order page
   await page.getByRole('button', { name: 'Order now' }).click();
 
